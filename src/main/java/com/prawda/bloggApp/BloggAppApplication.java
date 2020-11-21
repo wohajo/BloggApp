6662dae36d4eb85cf2e702983b2c28497037f278
Michał Prawda <m.prawda99@gmail.com>
2020-11-21 19:08:44 +0100

    Initial commit

---
+++ src/main/java/com/prawda/bloggApp/BloggAppApplication.java
@@ -0,0 +1,13 @@
+package com.prawda.bloggApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prawda/bloggApp/BloggAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prawda.bloggApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
