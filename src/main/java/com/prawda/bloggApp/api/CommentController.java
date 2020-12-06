dc30528249464285ac753610b32e163822d8c289
Michał Prawda <m.prawda99@gmail.com>
2020-12-06 19:20:45 +0100

    Add more REST functionalities to commment api

--- src/main/java/com/prawda/bloggApp/api/CommentController.java
+++ src/main/java/com/prawda/bloggApp/api/CommentController.java
@@ -4,6 +4,7 @@ package com.prawda.bloggApp.api;
 import com.prawda.bloggApp.domain.Comment;
 import com.prawda.bloggApp.service.CommentManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +34,10 @@ public class CommentController {
     @GetMapping("/api/comments/user/{username}")
     public List<Comment> getCommentsByUsername(@PathVariable String username) {
         return commentManager.findByUsername(username);
+    }
+
+    @DeleteMapping("api/comments/{id}")
+    public void deleteCommentById(@PathVariable String id) {
+        commentManager.remove(id);
     }
 }
